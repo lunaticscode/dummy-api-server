14d3da2742e20386bde993a52051b1670ef8699f
estsecurity <ispark_estsecurity@estsecurityui-MacBookPro.local>
2022-08-11 11:23:59 +0900

    mysql connection 방식 재변경( -> pool ), package.json 파일 수정

--- db_init.js
+++ db_init.js
@@ -1,4 +1,4 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const db_connection = mysql.createConnection(process.env.PLANET_DB_URL);
+const db_connection = mysql.createPool(process.env.PLANET_DB_URL);
 module.exports = db_connection;
